d2dc56126b69e267f5032b0401f6366d16ec80ea
TW Support <twsupport@DalLoanerLaptop5.local>
2018-06-02 13:08:08 -0500

    Score by velocity script, adds points based on velocity magnitude

---
+++ Assets/Scripts/ScoreByVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreByVelocity : MonoBehaviour {
+
+    private Rigidbody rb;
+    public int score;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+		
+	}
+
+    float GetVelocity () {
+        return rb.velocity.magnitude;
+    }
+	
+	// Update is called once sper frame
+	void FixedUpdate () {
+       
+        score += (int) GetVelocity();
+        Debug.Log(score);
+
+	}
+}
