0767cc84c49aae12c7d25acb42a441a42e4f8ca7
auroratide <tf.auroratide@gmail.com>
2018-06-02 10:08:52 -0500

    We can now move forward

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CabbyCoders.CrazyCab {
+  public class PlayerBehaviour : MonoBehaviour {
+
+    [SerializeField] private Config config;
+
+    private Rigidbody rb;
+
+    public void Start () {
+      rb = GetComponent<Rigidbody>();
+      rb.velocity = new Vector3(0, 0, config.startingSpeed);
+    }
+
+    [System.Serializable]
+    public class Config {
+      public float startingSpeed = 1.0f;
+    }
+
+  }
+}
