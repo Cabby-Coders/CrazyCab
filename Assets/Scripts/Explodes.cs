9fdb98137519bf5737ef0d867165cb9295d4f1ae
Jabox2 <jabox2@gmail.com>
2018-06-02 11:06:49 -0500

    Explodes on collision

---
+++ Assets/Scripts/Explodes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explodes : MonoBehaviour {
+
+    [SerializeField] private Config config;
+
+	public void OnCollisionEnter(Collision collision)
+	{
+        GameObject explosion = config.explosion;
+        Instantiate(explosion);
+        //explosion.AddComponent<Rigidbody>();
+        explosion.transform.position = this.transform.position;
+	}
+
+
+        [System.Serializable]
+    public class Config
+    {
+        public GameObject explosion;
+    }
+}
