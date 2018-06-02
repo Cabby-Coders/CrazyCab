3b5b0520fb389b08171477b0026a7afd7b150e11
Jabox2 <jabox2@gmail.com>
2018-06-02 14:14:28 -0500

    add score text

--- Assets/Scripts/ScoreByVelocity.cs
+++ Assets/Scripts/ScoreByVelocity.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ScoreByVelocity : MonoBehaviour {
 
     private Rigidbody rb;
     public int score;
+    [SerializeField] private Config config;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,10 @@ public class ScoreByVelocity : MonoBehaviour {
         score = 0;
 		
 	}
+
+    public int GetScore() {
+        return score;
+    }
 
     float GetVelocity () {
         return rb.velocity.magnitude;
@@ -23,6 +30,12 @@ public class ScoreByVelocity : MonoBehaviour {
        
         score += (int) GetVelocity();
         Debug.Log(score);
-
+        config.scoreText.text = GetScore().ToString();
 	}
+
+    [System.Serializable]
+    public class Config
+    {
+        public Text scoreText;
+    }
 }
