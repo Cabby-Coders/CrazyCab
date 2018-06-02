3f3ff57a42e2c3fb89d0ba64ac1fb1dd667b5f48
auroratide <tf.auroratide@gmail.com>
2018-06-02 17:35:50 -0500

    Main Menu

---
+++ Assets/Scripts/OnClickStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OnClickStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void startGame(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+  }
+}
