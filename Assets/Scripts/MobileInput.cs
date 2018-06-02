67bdd0e0a7b2f86ee0cf5ab287127677699ff406
TW Support <twsupport@DalLoanerLaptop5.local>
2018-06-02 14:25:06 -0500

    Mobile Input component

---
+++ Assets/Scripts/MobileInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : MonoBehaviour
+{
+
+    public float HorizontalAxis;
+
+    // Use this for initialization
+    void Start()
+    {
+        HorizontalAxis = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ((Application.platform == RuntimePlatform.IPhonePlayer) || (Application.platform == RuntimePlatform.IPhonePlayer))
+        {
+            HorizontalAxis = Input.acceleration.z;
+        }
+        else
+        {
+            HorizontalAxis = Input.GetAxis("Horizontal");
+        }
+    }
+}
